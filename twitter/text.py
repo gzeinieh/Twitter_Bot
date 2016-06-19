19cef4db8a1b9f489412226ef791d99f81d86a36
gzeinieh <zeiniehgeorge@gmail.com>
2016-06-18 20:22:52 -0500

    removed file text.py

--- twitter/text.py
+++
@@ -1,6 +0,0 @@
-from unidecode import decode
-
-
-with open( "/Users/George/PycharmProjects/Twitter_Bot/twitter/data/update_status_2016-06-14 18:20:15.247625.txt"
-        ,mode ='r', encoding='utf-8') as f:
-    print(f.read().decode('unicode_escape'))
